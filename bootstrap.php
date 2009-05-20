74d846e170ae3576a195b09ccae773c868469aa0
Marc-Henri PAMISEUX <mhp@libricks.fr>
2009-05-20 22:47:06 +0200

    Correctif concernant la licence GPL, il manquait une ligne

--- bootstrap.php
+++ bootstrap.php
@@ -21,6 +21,7 @@
  *  | Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne. |
  *  |                                                                      |
  *  | This program is free software; you can redistribute it and/or        |
+ *  | modify it under the terms of the GNU General Public License          |
  *  | as published by the Free Software Foundation; either version 2       |
  *  | of the License, or (at your option) any later version.               |
  *  |                                                                      |
